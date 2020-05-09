6d3050a34c23293e0b23f5f90e4e5ab11f6fb781
erabin05 <e.rabin05@gmail.com>
2020-05-09 13:34:49 +0200

    rotation for all number of cards

--- cards.js
+++ cards.js
@@ -6,14 +6,14 @@ window.onload = function(){
     elBoard.innerHTML = generateElCards(cards);
 };
 
-const generateElCards = (cards) => cards.reduce((acc, card, i) => acc + `<section class='card' ${rotationBis(cards, i)}></section>`, "")
+const generateElCards = (cards) => cards.reduce((acc, card, i) => acc + `<section class='card' ${cardRotation(cards, i)}></section>`, "")
 
-const cardRotation = (indexCard) => result = `style="transform:rotate(${indexCard%2 === 0 ? 10 : -10}deg)"`
-
-const rotationBis = (cards, indexCard) => {
-  const middle = cards.length/2 - 0.5;
-  const ratio = middle - indexCard;
-  const halfTheNumberOfCard = Math.floor(cards.length/2)
-  const angle = 15 / Math.ceil(halfTheNumberOfCard - Math.abs(ratio))
-  return `style="transform:rotate(${ratio > 0 ? "-" : ""}${angle}deg)"`
+const cardRotation = (cards, indexCard) => {
+    const lengthIsOdd = cards.length%2 === 0;
+    const middle = cards.length/2 - 0.5;
+    const ratio = middle - indexCard;
+    const halfTheNumberOfCard = Math.floor(cards.length/2);
+    const randomBetween10and15 = Math.floor(Math.random() * 6) + 10;
+    const angle = randomBetween10and15 / ((halfTheNumberOfCard - Math.abs(ratio)) + (lengthIsOdd ? 0.5 : 1));
+    return `style="transform:rotate(${ratio > 0 ? "-" : ""}${angle}deg)"`
 }
