e5f1cde690f7eb0bc1d7f3436d3182edd3e767ca
erabin05 <e.rabin05@gmail.com>
2020-05-09 14:20:14 +0200

    cards vertical position

--- cards.js
+++ cards.js
@@ -3,26 +3,29 @@
 
 window.onload = function(){
     const elBoard = document.getElementById("board");
-
     elBoard.style.width= `${cards.length * cardWitdhInPx}px`
-
     elBoard.innerHTML = generateElCards(cards);
 };
 
-const generateElCards = (cards) => cards.reduce((acc, card, i) => acc + `<section class='card' ${cardStyle(i)}></section>`, "");
+const generateElCards = (cards) => cards.reduce((acc, card, i) => acc + `<section class='card' ${cardStyle(cards, i)}></section>`, "");
 
-const cardStyle = (i) => `style="${cardRotation(cards, i)};${cardPositionHorizontaly(i)};${cardDepth(i)}"`;
+const cardStyle = (cards, i) => `style="${cardRotation(cards, i)};${cardPositionHorizontaly(i)};${cardPositionVerticaly(i)};${cardDepth(i)}"`;
 
 const cardRotation = (cards, indexCard) => {
     const lengthIsOdd = cards.length%2 === 0;
     const middle = cards.length/2 - 0.5;
     const ratio = middle - indexCard;
     const halfTheNumberOfCard = Math.floor(cards.length/2);
-    const randomBetween10and15 = Math.floor(Math.random() * 6) + 10;
-    const angle = randomBetween10and15 / ((halfTheNumberOfCard - Math.abs(ratio)) + (lengthIsOdd ? 0.5 : 1));
+    const angle = randomNumberBetween(10, 15) / ((halfTheNumberOfCard - Math.abs(ratio)) + (lengthIsOdd ? 0.5 : 1));
     return `transform:rotate(${ratio > 0 ? "-" : ""}${angle}deg)`
 };
 
-const cardPositionHorizontaly = (indexCard) => `left:${cardWitdhInPx * indexCard}`;
+const cardPositionHorizontaly = (indexCard) => `left:${cardWitdhInPx * indexCard}px`;
 
-const cardDepth = (indexCard) => `z-index:${indexCard%2 === 0 ? 100 : 200}`
+const cardPositionVerticaly = (indexCard) => {
+    return `top:${(indexCard%2 === 0 ? randomNumberBetween(60, 120) : randomNumberBetween(0, 10))}px`
+}
+
+const cardDepth = (indexCard) => `z-index:${indexCard%2 === 0 ? 100 : 200}`;
+
+const randomNumberBetween = (min, max) => Math.floor(Math.random() * (max-min)+1) + min;
